1f5482a67833b3dec03fc1d95ecece8dbedcc43c
USC-Sumedha <USC-Sumedha@users.noreply.github.com>
2016-02-15 19:58:25 -0800

    Update size_count.py

--- size_count.py
+++ size_count.py
@@ -8,7 +8,6 @@ Word Count Example in the Simple Python MapReduce Framework
 mr = MapReduce.MapReduce()
 
 # =============================
-# Do not modify above this line
 
 def mapper(record):
     # key: document identifier
@@ -59,7 +58,6 @@ def reducer(key, list_of_values):
 
     mr.emit((key, finalOutput))  
 
-# Do not modify below this line
 # =============================
 if __name__ == '__main__':
   inputdata = open(sys.argv[1])
